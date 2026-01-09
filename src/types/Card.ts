b353aa119e0a95cfeaed080b0812847e1c8790d9
sagarshelke1995 <sagarshelke1995@gmail.com>
2026-01-09 17:53:19 +0530

    updated heading, button components

---
+++ src/types/Card.ts
@@ -0,0 +1,13 @@
+export interface CardData {
+  title: string;
+  category: string;
+  hero: string;
+  timing?: string;
+  Address?: string;
+  author: {
+    name: string;
+    avatar: string;
+    time?: string;
+  };
+  time:String;
+}
